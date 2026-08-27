44eb1481c28f4ced50795ef4615f495b16637387
alexey-va <freedeeml@gmail.com>
2026-08-27 15:57:02 +0300

    Accept both isolated lab origins

--- src/test/kotlin/ru/ruscrafting/farms/config/ArcFarmsConfigTest.kt
+++ src/test/kotlin/ru/ruscrafting/farms/config/ArcFarmsConfigTest.kt
@@ -721,7 +721,7 @@ class ArcFarmsConfigTest : FunSpec({
         val settings = ArcFarmsConfig.inspect(root)
 
         settings.serverId shouldBe "lab"
-        settings.network.allowedOrigins shouldBe setOf("lab")
+        settings.network.allowedOrigins shouldBe setOf("lab", "lab2")
         settings.network.playerAnnouncementsEnabled shouldBe false
         settings.debug.enabled shouldBe true
         settings.destinations.getValue("farm").server shouldBe "lab"
