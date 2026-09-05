89c8c0fc0559b5a154f45ccc2921f0a95c2174a5
alexey-va <freedeeml@gmail.com>
2026-09-05 04:34:24 +0300

    chore: publish enterprise changes as ArcFarms 0.31.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     jacoco
 }
 group = "ru.ruscrafting"
-version = "0.30.0"
+version = "0.31.0"
 description = "Shared farm, lumbermill, and mine activities for RusCrafting"
 
 val integrationTestSourceSet = sourceSets.create("integrationTest") {
