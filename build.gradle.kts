3317e6aea1a413341a229c311e856856e0294647
alexey-va <freedeeml@gmail.com>
2026-09-10 04:12:03 +0300

    Clarify mine cargo and rescue destinations

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     jacoco
 }
 group = "ru.ruscrafting"
-version = "0.40.6"
+version = "0.40.7"
 description = "Shared farm, lumbermill, and mine activities for RusCrafting"
 
 val integrationTestSourceSet = sourceSets.create("integrationTest") {
