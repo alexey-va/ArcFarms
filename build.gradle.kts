dceb2c19b4cb35dd8b6aaa3aa422ed9d649825c8
alexey-va <freedeeml@gmail.com>
2026-09-20 05:01:18 +0300

    Align shared core dependencies on 2.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
 java { toolchain { languageVersion.set(JavaLanguageVersion.of(25)) } }
 kotlin { jvmToolchain(25) }
 
-val arcCoreVersion = "2.7.9"
+val arcCoreVersion = "2.7.10"
 
 dependencies {
     implementation(kotlin("stdlib"))
